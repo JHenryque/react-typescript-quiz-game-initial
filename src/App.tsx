d3db45f9d62e9119c58fc2440fea130eb910a34b
JHenrique <henryque.pc.game@gmail.com>
2025-04-09 16:31:56 -0300

    Ajuste do Tipo do Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import Game from "./components/Game.tsx";
 import { useQuixContext } from "./QuixContext.tsx";
 
 function App() {
-  const state = useQuixContext();
+  const { state, dispatch } = useQuixContext();
   console.log(state);
 
   return (
     <>
       <Score />
       <Game />
+      <h2>Status: {state.gameStatus} </h2>
+      <button
+        onClick={() => dispatch({ type: "setStatus", payload: "fetching" })}
+      >
+        Ser Loading
+      </button>
     </>
   );
 }
--- src/QuixContext.tsx
+++ src/QuixContext.tsx
@@ -1,18 +1,44 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+interface QuizUseContext {
+  state: QuizState;
+  dispatch: React.Dispatch<QuizAction>;
+}
 
 interface QuizState {
-  gameStatus: "idle" | "fetching" | "ready";
+  gameStatus: Status;
 }
+
+type Status = "idle" | "fetching" | "ready";
+
+type QuizAction = { type: "reset" } | { type: "setStatus"; payload: Status };
 
 const initialState: QuizState = {
   gameStatus: "idle",
 };
 
-const QuizContext = createContext<QuizState>(initialState);
+const QuizContext = createContext<QuizUseContext>({
+  state: initialState,
+  dispatch: () => null,
+});
 
 export function QuizProvider({ children }: { children: React.ReactElement }) {
-  const [state, setState] = useState(initialState);
-  return <QuizContext.Provider value={state}>{children}</QuizContext.Provider>;
+  const [state, dispatch] = useReducer(QuizReducer, initialState);
+
+  return (
+    <QuizContext.Provider value={{ state, dispatch }}>
+      {children}
+    </QuizContext.Provider>
+  );
 }
 
 export const useQuixContext = () => useContext(QuizContext);
+
+function QuizReducer(state: QuizState, action: QuizAction): QuizState {
+  switch (action.type) {
+    case "setStatus":
+      return { ...state, gameStatus: action.payload };
+    default:
+      throw new Error("Unknown action");
+  }
+}
