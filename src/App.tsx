0b64c0c961bfe706a8ece059efd5be8f2914d341
JHenrique <henryque.pc.game@gmail.com>
2025-04-10 12:26:07 -0300

    Adicionando a Pergunta ao Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
 import "./App.scss";
+import FullPageLoader from "./components/FullPageLoader.tsx";
 import Score from "./components/Score.tsx";
 import Game from "./components/Game.tsx";
-import { useQuixContext } from "./QuixContext.tsx";
+import { useQuixContext, Question, QuizResponse } from "./QuixContext.tsx";
+import { useEffect } from "react";
 
 function App() {
   const { state, dispatch } = useQuixContext();
-  console.log(state);
+
+  async function fetchQuestion() {
+    try {
+      dispatch({ type: "setStatus", payload: "fetching" });
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=20&category=18"
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const data: QuizResponse = await response.json();
+
+      if (data.response_code === 0) {
+        const question: Question = data.results[0];
+        dispatch({ type: "setStatus", payload: "ready" });
+        dispatch({ type: "setQuestion", payload: question });
+      } else {
+        dispatch({ type: "setStatus", payload: "error" });
+      }
+    } catch (error) {
+      console.log("error: ", error);
+      dispatch({ type: "setStatus", payload: "error" });
+    }
+  }
+
+  useEffect(() => {
+    if (state.gameStatus === "idle") {
+      fetchQuestion().catch((err) => console.log(err));
+    }
+  });
 
   return (
     <>
-      <Score />
-      <Game />
-      <h2>Status: {state.gameStatus} </h2>
-      <button
-        onClick={() => dispatch({ type: "setStatus", payload: "fetching" })}
-      >
-        Ser Loading
-      </button>
+      {state.gameStatus === "fetching" ? (
+        <FullPageLoader />
+      ) : state.gameStatus === "error" ? (
+        <p>Something went wrong</p>
+      ) : state.gameStatus === "ready" ? (
+        <>
+          <Score />
+          <Game />
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
--- src/QuixContext.tsx
+++ src/QuixContext.tsx
@@ -1,20 +1,38 @@
 import { createContext, useContext, useReducer } from "react";
 
+export interface Question {
+  type: "multiple" | "boolean";
+  difficulty: "easy" | "medium" | "hard";
+  category: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
+
+export interface QuizResponse {
+  response_code: number;
+  results: Question[];
+}
+
 interface QuizUseContext {
   state: QuizState;
   dispatch: React.Dispatch<QuizAction>;
 }
 
 interface QuizState {
+  question: Question | null;
   gameStatus: Status;
 }
 
-type Status = "idle" | "fetching" | "ready";
+type Status = "idle" | "fetching" | "ready" | "error";
 
-type QuizAction = { type: "reset" } | { type: "setStatus"; payload: Status };
+type QuizAction =
+  | { type: "setStatus"; payload: Status }
+  | { type: "setQuestion"; payload: Question };
 
 const initialState: QuizState = {
   gameStatus: "idle",
+  question: null,
 };
 
 const QuizContext = createContext<QuizUseContext>({
@@ -36,6 +54,8 @@ export const useQuixContext = () => useContext(QuizContext);
 
 function QuizReducer(state: QuizState, action: QuizAction): QuizState {
   switch (action.type) {
+    case "setQuestion":
+      return { ...state, question: action.payload };
     case "setStatus":
       return { ...state, gameStatus: action.payload };
     default:
